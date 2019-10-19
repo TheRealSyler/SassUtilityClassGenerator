c78c2d4a6d166f984b2490c151bc6b71d6deb7de
syler <hileo77@gmail.com>
2019-10-19 15:20:25 +0200

    added sizes (height / width)

--- SassUtilityClassesGenerator.ts
+++ SassUtilityClassesGenerator.ts
@@ -17,13 +17,13 @@ function GenerateMP() {
   for (const step of steps) {
     for (const key in MPData) {
       if (MPData.hasOwnProperty(key)) {
-        AddToDoc(`${key[0]}${step.toString().replace(/0./, '-')}`, key, `${step}rem`);
-        res += `.${key[0]}${step.toString().replace(/0./, '-')}
+        AddToDoc(`${key[0]}${GetStep(step)}`, key, `${step}rem`);
+        res += `.${key[0]}${GetStep(step)}
   ${key}: ${step}rem !important
 `;
         for (const prop of MPData[key]) {
-          AddToDoc(`${key[0]}${prop[0]}${step.toString().replace(/0./, '-')}`, `${key}-${prop}`, `${step}rem`);
-          res += `.${key[0]}${prop[0]}${step.toString().replace(/0./, '-')}
+          AddToDoc(`${key[0]}${prop[0]}${GetStep(step)}`, `${key}-${prop}`, `${step}rem`);
+          res += `.${key[0]}${prop[0]}${GetStep(step)}
   ${key}-${prop}: ${step}rem !important
 `;
         }
@@ -115,16 +115,49 @@ function GenerateCursor() {
 `;
   }
 }
+const sizeTypes = ['height', 'width'];
+const sizes = [
+  {
+    unit: 'rem',
+    steps
+  },
+  {
+    unit: '',
+    steps: [0]
+  },
+  {
+    unit: '%',
+    steps: [10, 25, 50, 75, 100, 125, 150, 200]
+  }
+];
+function GenerateSizeStuff() {
+  for (const type of sizeTypes) {
+    for (const size of sizes) {
+      for (const step of size.steps) {
+        AddToDoc(`${type}${GetStep(step)}${size.unit.replace('%', 'percent')}`, type, `${step}${size.unit}`);
+        res += `.${type}${GetStep(step)}${size.unit.replace('%', 'percent')}
+  ${type}: ${step}${size.unit} !important
+`;
+      }
+    }
+  }
+}
 
 GenerateMP();
 GenerateFlex();
 GeneratePos();
 GenerateCursor();
+GenerateSizeStuff();
 
 function AddToDoc(name: string, prop: string, value: string) {
   const desc = `Sets the \`${prop}\` property to \`${value}\``;
   doc += `|${name}${' '.repeat(Math.max(0, 24 - name.length))}|${desc}${' '.repeat(Math.max(0, 96 - desc.length))}|
 `;
 }
+
+function GetStep(step: number) {
+  return step.toString().replace(/0\./, '-');
+}
+
 writeFileSync('./utils.sass', res);
 writeFileSync('./README.md', doc);
